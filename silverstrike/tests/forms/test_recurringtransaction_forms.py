41d6ea6222475087b362a6bf6c397b0f306ee5a8
Simon Hanna <simhnna@gmail.com>
2017-10-08 23:20:19 +0200

    add tests

--- silverstrike/tests/forms/test_recurringtransaction_forms.py
+++ silverstrike/tests/forms/test_recurringtransaction_forms.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 
 from silverstrike.forms import RecurringTransactionForm
-from silverstrike.models import Account, Transaction
+from silverstrike.models import Account, RecurringTransaction, Transaction
 
 
 class RecurringTransactionFormTests(TestCase):
@@ -17,7 +17,59 @@ class RecurringTransactionFormTests(TestCase):
         personal = Account.objects.create(name='foo')
         other = Account.objects.create(name='bar')
         form = RecurringTransactionForm({
-            'amount': -100, 'transaction_type': Transaction.TRANSFER,
-            'src': personal, 'dst': other})
+            'amount': -100, 'transaction_type': Transaction.TRANSFER, 'date': '2100-01-01',
+            'src': personal, 'dst': other, 'recurrence': RecurringTransaction.MONTHLY,
+            'title': 'foo'})
         self.assertFalse(form.is_valid())
+        self.assertEquals(len(form.errors), 1)
         self.assertIn('amount', form.errors)
+
+    def test_past_date(self):
+        personal = Account.objects.create(name='foo')
+        other = Account.objects.create(name='bar')
+        form = RecurringTransactionForm({
+            'amount': 100, 'transaction_type': Transaction.TRANSFER,
+            'src': personal, 'dst': other, 'date': '2016-01-01',
+            'recurrence': RecurringTransaction.MONTHLY, 'title': 'foo'})
+        self.assertFalse(form.is_valid())
+        self.assertEquals(len(form.errors), 1)
+        self.assertIn('date', form.errors)
+
+    def test_transfer_with_existing_accounts(self):
+        personal = Account.objects.create(name='foo')
+        other = Account.objects.create(name='bar')
+        form = RecurringTransactionForm({
+            'amount': 100, 'transaction_type': Transaction.TRANSFER, 'date': '2100-01-01',
+            'src': personal, 'dst': other, 'recurrence': RecurringTransaction.MONTHLY,
+            'title': 'foo'})
+        self.assertTrue(form.is_valid())
+        transaction = form.save()
+        self.assertIsNotNone(transaction)
+        self.assertEquals(transaction.src, personal)
+        self.assertEquals(transaction.dst, other)
+
+    def test_withdraw_with_existing_accounts(self):
+        personal = Account.objects.create(name='foo')
+        other = Account.objects.create(name='bar', account_type=Account.EXPENSE)
+        form = RecurringTransactionForm({
+            'amount': 100, 'transaction_type': Transaction.WITHDRAW, 'date': '2100-01-01',
+            'src': personal, 'dst': other, 'recurrence': RecurringTransaction.MONTHLY,
+            'title': 'foo'})
+        self.assertTrue(form.is_valid())
+        transaction = form.save()
+        self.assertIsNotNone(transaction)
+        self.assertEquals(transaction.src, personal)
+        self.assertEquals(transaction.dst, other)
+
+    def test_deposit_with_existing_accounts(self):
+        personal = Account.objects.create(name='foo')
+        other = Account.objects.create(name='bar', account_type=Account.REVENUE)
+        form = RecurringTransactionForm({
+            'amount': 100, 'transaction_type': Transaction.DEPOSIT, 'date': '2100-01-01',
+            'src': other, 'dst': personal, 'recurrence': RecurringTransaction.MONTHLY,
+            'title': 'foo'})
+        self.assertTrue(form.is_valid())
+        transaction = form.save()
+        self.assertIsNotNone(transaction)
+        self.assertEquals(transaction.src, other)
+        self.assertEquals(transaction.dst, personal)
