f7f5d4b634acd774782a5d3a6f3c12694ab825f0
Simon Hanna <simhnna@gmail.com>
2017-10-31 17:21:54 +0100

    Make firefly import independent of column sorting

--- silverstrike/lib.py
+++ silverstrike/lib.py
@@ -56,35 +56,14 @@ def import_csv(path, config):
 
 
 def import_firefly(csv_file):
-    # journal_id = 0
-    # transaction_id = 1
-    date = 2
-    description = 3
-    # currency_code = 4
-    amount = 5
-    # foreign_currency = 6
-    # foreign_amount = 7
-    transaction_type = 8
-    # source_account_id = 9
-    source_account_name = 10
-    # source_account_iban = 11
-    # source_account_bic = 12
-    # source_account_number = 13
-    # source_account_currency = 14
-    # destination_account_id = 15
-    destination_account_name = 16
-    # destination_account_iban = 17
-    # destination_account_bic = 18
-    # destination_account_number = 19
-    # destination_account_currency = 20
-    # budget_id = 21
-    # budget_name = 22
-    # category_id = 23
-    category_name = 24
-    # bill_id = 25
-    # bill_name = 26
-    # notes = 27
-    # tags = 28
+    date = 'date'
+    title = 'description'
+    amount = 'amount'
+    source = 'asset_account_name'
+    destination = 'opposing_account_name'
+    category = 'category_name'
+    notes = 'notes'
+    transaction_type = 'transaction_type'
 
     system_account, _ = Account.objects.get_or_create(name='system', account_type=Account.SYSTEM)
 
@@ -107,71 +86,80 @@ def import_firefly(csv_file):
     for line in csv.reader(open(csv_file)):
         if first_time:
             first_time = False
+            line = {k: v for v, k in enumerate(line)}
+            date = line[date]
+            title = line[title]
+            amount = line[amount]
+            transaction_type = line[transaction_type]
+            source = line[source]
+            destination = line[destination]
+            category = line[category]
+            notes = line[notes]
             continue
-        if line[source_account_name] in personal_accounts:
-                line[source_account_name] = personal_accounts[line[source_account_name]]
+        if line[source] in personal_accounts:
+                line[source] = personal_accounts[line[source]]
         else:
-            a = Account.objects.create(name=line[source_account_name],
+            a = Account.objects.create(name=line[source],
                                        account_type=Account.PERSONAL)
             personal_accounts[a.name] = a.id
-            line[source_account_name] = a.id
+            line[source] = a.id
 
         line[amount] = float(line[amount])
 
         if line[transaction_type] == 'Withdrawal':
             t_type = Transaction.WITHDRAW
-            if line[destination_account_name] in expense_accounts:
-                line[destination_account_name] = expense_accounts[line[destination_account_name]]
+            if line[destination] in expense_accounts:
+                line[destination] = expense_accounts[line[destination]]
             else:
-                a = Account.objects.create(name=line[destination_account_name],
+                a = Account.objects.create(name=line[destination],
                                            account_type=Account.EXPENSE)
                 expense_accounts[a.name] = a.id
-                line[destination_account_name] = a.id
+                line[destination] = a.id
 
         elif line[transaction_type] == 'Transfer':
             # positive transfers are wrong
             if line[amount] > 0:
                 continue
             t_type = Transaction.TRANSFER
-            if line[destination_account_name] in personal_accounts:
-                line[destination_account_name] = personal_accounts[line[destination_account_name]]
+            if line[destination] in personal_accounts:
+                line[destination] = personal_accounts[line[destination]]
             else:
-                a = Account.objects.create(name=line[destination_account_name],
+                a = Account.objects.create(name=line[destination],
                                            account_type=Account.PERSONAL)
                 personal_accounts[a.name] = a.id
-                line[destination_account_name] = a.id
+                line[destination] = a.id
 
         elif line[transaction_type] == 'Deposit':
             t_type = Transaction.DEPOSIT
-            if line[destination_account_name] in revenue_accounts:
-                line[destination_account_name] = revenue_accounts[line[destination_account_name]]
+            if line[destination] in revenue_accounts:
+                line[destination] = revenue_accounts[line[destination]]
             else:
-                a = Account.objects.create(name=line[destination_account_name],
+                a = Account.objects.create(name=line[destination],
                                            account_type=Account.REVENUE)
                 revenue_accounts[a.name] = a.id
-                line[destination_account_name] = a.id
+                line[destination] = a.id
         elif line[transaction_type] == 'Opening balance':
-            line[destination_account_name] = system_account.id
+            line[destination] = system_account.id
 
-        if line[category_name] in categories:
-            line[category_name] = categories[line[category_name]]
-        elif line[category_name]:
-            c = Category.objects.create(name=line[category_name])
+        if line[category] in categories:
+            line[category] = categories[line[category]]
+        elif line[category]:
+            c = Category.objects.create(name=line[category])
             categories[c.name] = c.id
-            line[category_name] = c.id
+            line[category] = c.id
         else:
-            line[category_name] = None
+            line[category] = None
         line[date] = datetime.datetime.strptime(line[date], '%Y%m%d')
 
         transaction = Transaction.objects.create(
-            title=line[description], date=line[date],
+            title=line[title], date=line[date],
             transaction_type=t_type)
         Split.objects.bulk_create(
-            [Split(account_id=line[source_account_name], title=line[description],
+            [Split(account_id=line[source], title=line[title],
                    date=line[date],
-                   opposing_account_id=line[destination_account_name], amount=line[amount],
-                   transaction_id=transaction.id, category_id=line[category_name]),
-             Split(account_id=line[destination_account_name], title=line[description],
+                   opposing_account_id=line[destination], amount=line[amount],
+                   transaction_id=transaction.id, category_id=line[category]),
+             Split(account_id=line[destination], title=line[title],
                    date=line[date],
-                   opposing_account_id=line[source_account_name], amount=-line[amount],
-                   transaction_id=transaction.id, category_id=line[category_name])])
+                   opposing_account_id=line[source], amount=-line[amount],
+                   transaction_id=transaction.id, category_id=line[category])])
