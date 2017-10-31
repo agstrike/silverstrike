c298fb9563b148947019c07cf87adeff44e1624e
Simon Hanna <simhnna@gmail.com>
2017-10-31 17:37:17 +0100

    Add test for firefly import

---
+++ silverstrike/tests/test_import.py
@@ -0,0 +1,17 @@
+import os
+
+from django.test import TestCase
+
+from silverstrike.lib import import_firefly
+
+
+class FireFlyImportTests(TestCase):
+    def setUp(self):
+        super(FireFlyImportTests, self).setUp()
+        self.csv_file = os.path.join(
+            os.path.dirname(__file__),
+            'fixtures',
+            'firefly.csv')
+
+    def test_single_import(self):
+        import_firefly(self.csv_file)
