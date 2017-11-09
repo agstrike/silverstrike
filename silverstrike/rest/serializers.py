72a25e001187d5ae4455009fcf8bd31a87289784
Simon Hanna <simhnna@gmail.com>
2017-11-09 18:07:31 +0100

    Basic validation on ransaction creation

--- silverstrike/rest/serializers.py
+++ silverstrike/rest/serializers.py
@@ -6,7 +6,7 @@ from silverstrike.models import Account, Category, Transaction, Split
 
 
 
-class AccountSerializer(serializers.HyperlinkedModelSerializer):
+class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
         fields = ('id', 'name', 'account_type', 'active', 'show_on_dashboard')
@@ -17,21 +17,35 @@ class AccountSerializer(serializers.HyperlinkedModelSerializer):
         return value
 
 
-class TransactionSerializer(serializers.HyperlinkedModelSerializer):
-    splits = serializers.HyperlinkedIdentityField(many=True, read_only=True, view_name='split-detail')
-
-    class Meta:
-        model = Transaction
-        fields = ('id', 'title', 'date', 'transaction_type', 'splits')
-
-
-class SplitSerializer(serializers.HyperlinkedModelSerializer):
+class SplitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Split
         fields = ('id', 'title', 'date', 'account', 'opposing_account',
                   'amount', 'category', 'transaction')
 
-class CategorySerializer(serializers.HyperlinkedModelSerializer):
+
+class TransactionSerializer(serializers.ModelSerializer):
+    #splits = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Transaction
+        fields = ('id', 'title', 'date', 'transaction_type', 'splits')
+
+    splits = SplitSerializer(many=True)
+
+    def create(self, validated_data):
+        split_sum = sum([split['amount'] for split in validated_data['splits']])
+        if split_sum != 0:
+            raise serializers.ValidationError("The sum of splits does not balance")
+        split_data = validated_data.pop('splits')
+        transaction = Transaction.objects.create(**validated_data)
+
+        for split in split_data:
+            Split.objects.create(transaction=transaction, **split)
+        return transaction
+
+
+class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ('id', 'name')
