926dd737a97cee1d7bf0c07806d7564e2f9797f1
Simon Hanna <simhnna@gmail.com>
2017-11-09 21:36:54 +0100

    Allow updating transactions

--- silverstrike/rest/serializers.py
+++ silverstrike/rest/serializers.py
@@ -20,6 +20,9 @@ class SplitSerializer(serializers.ModelSerializer):
         fields = ('id', 'title', 'date', 'account', 'opposing_account',
                   'amount', 'category', 'transaction')
 
+    # needed so that the id is transfered for update actions
+    id = serializers.IntegerField(required=False)
+
 
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
@@ -28,16 +31,40 @@ class TransactionSerializer(serializers.ModelSerializer):
 
     splits = SplitSerializer(many=True)
 
-    def create(self, validated_data):
-        split_sum = sum([split['amount'] for split in validated_data['splits']])
+    def validate(self, data):
+        split_sum = sum([split['amount'] for split in data['splits']])
         if split_sum != 0:
             raise serializers.ValidationError("The sum of splits does not balance")
+        return data
+
+    def create(self, validated_data):
         split_data = validated_data.pop('splits')
         transaction = Transaction.objects.create(**validated_data)
 
         for split in split_data:
             Split.objects.create(transaction=transaction, **split)
         return transaction
+
+    def update(self, instance, validated_data):
+        # update transaction
+        instance.title = validated_data.get('title', instance.title)
+        instance.date = validated_data.get('date', instance.date)
+        instance.transaction_type = validated_data.get('transaction_type',
+                                                       instance.transaction_type)
+        instance.save()
+        # update splits
+        for split in validated_data.get('splits'):
+            split_object = Split.objects.get(id=split.get('id'))
+
+            split_object.title = split.get('title', split_object.title)
+            split_object.date = split.get('date', split_object.date)
+            split_object.account = split.get('account', split_object.account)
+            split_object.opposing_account = split.get('opposing_account',
+                                                      split_object.opposing_account)
+            split_object.amount = split.get('amount', split_object.amount)
+            split_object.category = split.get('category', split_object.category)
+            split_object.save()
+        return instance
 
 
 class CategorySerializer(serializers.ModelSerializer):
