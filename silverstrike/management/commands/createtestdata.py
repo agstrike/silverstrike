d4c642dc803c71e8ff6bbc6b69db7a4b3d090059
Simon Hanna <simhnna@gmail.com>
2017-12-19 21:48:51 +0100

    Update command to create test data

--- silverstrike/management/commands/createtestdata.py
+++ silverstrike/management/commands/createtestdata.py
@@ -1,15 +1,18 @@
 from datetime import date
 from random import randrange
 
+from dateutil.relativedelta import relativedelta
+
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.management.base import BaseCommand
 
-from silverstrike.models import Account, Category, Split, Transaction
+from silverstrike.models import Account, Category, RecurringTransaction, Split, Transaction
 
 
-def _create_transaction(date, amount, src, dst, title, category, type):
+def _create_transaction(date, amount, src, dst, title, category, type, recurrence):
     transaction = Transaction.objects.create(title=title, date=date,
-                                             transaction_type=type)
+                                             transaction_type=type,
+                                             recurrence=recurrence)
     Split.objects.create(account=src, opposing_account=dst, transaction=transaction,
                          amount=-amount, category=category, date=date, title=title)
     Split.objects.create(account=dst, opposing_account=src, transaction=transaction,
@@ -19,26 +22,16 @@ def _create_transaction(date, amount, src, dst, title, category, type):
 class Command(BaseCommand):
     def handle(self, *args, **options):
         self._initialize()
-        try:
-            past = Transaction.objects.latest('date')
-            start_month = past.date.month + 1
-            start_year = past.date.year
-            if start_month == 13:
-                start_year += 1
-        except ObjectDoesNotExist:
-            start_year = 2016
-            start_month = 1
-
         today = date.today()
-        for y in range(start_year, today.year + 1):
-            if y == start_year and y == today.year:
-                self._create_yearly(y, start_month, today.month)
-            if y == start_year:
-                self._create_yearly(y, start_month, 13)
-            elif y == today.year:
-                self._create_yearly(y, 1, today.month)
-            else:
-                self._create_yearly(y, 1, 13)
+        try:
+            start_date = Transaction.objects.filter(
+                date__lte=today).latest('date').date.replace(day=1)
+        except ObjectDoesNotExist:
+            start_date = date(2016, 1, 1)
+
+        while start_date < today:
+            self._create_monthly(start_date.year, start_date.month)
+            start_date += relativedelta(months=+1)
 
     def _initialize(self):
         D = Transaction.DEPOSIT
@@ -62,31 +55,32 @@ class Command(BaseCommand):
         self.insurance, _ = Category.objects.get_or_create(name='insurance')
         self.leisure, _ = Category.objects.get_or_create(name='leisure')
 
-        self.YEARLY = [
-            ('special expense', [2, 4, 6, 8, 10, 12], self.checking, self.supermarket,
-             [100, 500], self.groceries, W),
-        ]
+        self.rent, _ = RecurringTransaction.objects.update_or_create(
+            title='rent', src=self.checking, dst=self.landlord,
+            defaults={'recurrence': RecurringTransaction.MONTHLY,
+                      'transaction_type': W, 'category': self.home, 'amount': 900,
+                      'date': (date.today() + relativedelta(months=+1)).replace(day=2)})
+        self.insurnace_r, _ = RecurringTransaction.objects.update_or_create(
+            title='insurance', src=self.checking, dst=self.insurer,
+            defaults={'recurrence': RecurringTransaction.MONTHLY,
+                      'transaction_type': W, 'category': self.insurance, 'amount': 70,
+                      'date': (date.today() + relativedelta(months=+1)).replace(day=15)})
 
         self.MONTHLY = [
-            ('income', [1], self.work, self.checking, [2000, 3000], None, D),
-            ('rent', [2], self.checking, self.landlord, [900, 901], self.home, W),
-            ('saving', [26], self.checking, self.savings, [100, 500], None, T),
-            ('groceries', [1, 5, 10, 15, 20, 25, 27], self.checking, self.supermarket,
-             [50, 100], self.groceries, W),
-            ('insurance', [15], self.checking, self.insurer, [70, 200], self.insurance, W),
-            ('clubbing', [3, 8, 17, 22, 27], self.checking, self.club, [20, 100], self.leisure, W),
+            ('income', [1], self.work, self.checking, [2000, 2500], None, D, None),
+            ('rent', [2], self.checking, self.landlord, [900, 901], self.home, W, self.rent),
+            ('saving', [27], self.checking, self.savings, [100, 300], None, T, None),
+            ('groceries', [1, 5, 10, 13, 15, 20, 21, 25, 27], self.checking, self.supermarket,
+             [50, 100], self.groceries, W, None),
+            ('insurance', [15], self.checking, self.insurer, [70, 200], self.insurance, W,
+             self.insurnace_r),
+            ('clubbing', [3, 8, 17, 22, 27], self.checking, self.club, [20, 100], self.leisure, W,
+             None),
         ]
 
     def _create_monthly(self, year, month):
-        for title, days, src, dst, amountRange, category, type in self.MONTHLY:
+        for title, days, src, dst, amountRange, category, type, recurrence in self.MONTHLY:
             for day in days:
                 amount = randrange(amountRange[0], amountRange[1])
-                _create_transaction(date(year, month, day), amount, src, dst, title, category, type)
-
-    def _create_yearly(self, year, start, end):
-        for title, months, src, dst, amountRange, category, type in self.YEARLY:
-            for m in range(start, end):
-                self._create_monthly(year, m)
-                if m in months:
-                    amount = randrange(amountRange[0], amountRange[1])
-                    _create_transaction(date(year, m, 1), amount, src, dst, title, category, type)
+                _create_transaction(date(year, month, day), amount, src, dst,
+                                    title, category, type, recurrence)
