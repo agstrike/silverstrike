92f124d652710bed3e2f252d2c9c53086bea8efd
Simon Hanna <simhnna@gmail.com>
2021-10-27 23:46:31 +0200

    Include Dockerfile in main repo

---
+++ docker/wsgi.py
@@ -0,0 +1,17 @@
+"""
+WSGI config for demo project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+from whitenoise import WhiteNoise
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+application = get_wsgi_application()
+application = WhiteNoise(application, root='./static')
