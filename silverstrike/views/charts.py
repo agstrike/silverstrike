519936cbac54af30861e58c5002a9a275f27b899
qedi-r <1435081+qedi-r@users.noreply.github.com>
2022-06-28 19:35:29 -0400

    fix charts page crashing when no transactions exist

--- silverstrike/views/charts.py
+++ silverstrike/views/charts.py
@@ -19,8 +19,10 @@ class ChartView(LoginRequiredMixin, generic.TemplateView):
         context['minus_3_months'] = date.today() - relativedelta(months=3)
         context['minus_6_months'] = date.today() - relativedelta(months=6)
         context['minus_12_months'] = date.today() - relativedelta(years=1)
-        earliest_transaction = Transaction.objects.earliest('date')
-        context['all_time'] = earliest_transaction.date - relativedelta(days=1)
+        context['all_time'] = date.today()
+        if Transaction.objects.first():
+            earliest_transaction = Transaction.objects.earliest('date')
+            context['all_time'] = earliest_transaction.date - relativedelta(days=1)
 
         context['first_day_of_month'] = date.today().replace(day=1)
         context['last_day_of_month'] = last_day_of_month(date.today())
